35ba28e649f06e6d1d1332241ac3b0d3ecf97ec0
krishjais98 <preetijaiswal983@gmail.com>
2026-03-17 00:11:33 +0530

    1auto

--- Number_Divisible_by_3_&_5.cpp
+++ Number_Divisible_by_3_&_5.cpp
@@ -8,6 +8,6 @@ if(n%3==0 or n%5==0){
     cout<<"Number is Divisible by either by 3 or 5";
 }
 else{
-    cout<<"Number is Neither Divisible By 3 or 5"<<endl;
+    cout<<"Number is Neither Divisible By 3 or 5";
 }
 }
